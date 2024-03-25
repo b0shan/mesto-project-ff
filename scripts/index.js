26bb94e164db3cf73c093a0ddbb9bd2d8ce88a8a
boshan <ziijey@yandex.ru>
2024-03-25 12:25:48 +0300

    0.7 js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
+
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const places = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__image').src = link;
+    card.querySelector('.card__title').textContent = name;
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+    return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const deleteItem = event.target.closest('.card');
+    deleteItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    const card = createCard(item.name, item.link, deleteCard);
+    places.append(card);
+});
+
+    

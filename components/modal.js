a0568580ec81f17556cb06ed202cb71da40d9d39
Georgy <146881586+b0shan@users.noreply.github.com>
2024-06-08 02:53:53 +0300

    rev1

---
+++ components/modal.js
@@ -0,0 +1,17 @@
+//открытие закрытие попапа
+export function openModal(modal) {
+    modal.classList.add("popup_is-opened");
+    window.addEventListener("keydown", closeEscape);
+}
+
+export function closeModal(modal) {
+    modal.classList.remove("popup_is-opened");
+    window.removeEventListener("keydown", closeEscape);
+}
+
+function closeEscape(evt) {
+    if (evt.code === "Escape") {
+      const item = document.querySelector(".popup_is-opened");
+      closeModal(item);
+    }
+}

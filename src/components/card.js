d0d3294e9576cf5e3648c08b780e4b050990d151
boshan <ziijey@yandex.ru>
2024-06-09 19:05:26 +0300

    44323

--- src/components/card.js
+++ src/components/card.js
@@ -1,32 +1,30 @@
-
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const places = document.querySelector('.places__list');
+export const places = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(name, link, deleteCard) {
+export function createCard(name, link, likeCard, deleteCard, handleImageClick) {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = card.querySelector('.card__delete-button');
     const image = card.querySelector('.card__image');
     image.src = link;
     card.querySelector('.card__title').textContent = name;
     image.setAttribute('alt', `Пейзаж ${name}`);
+    card.querySelector(".card__like-button").addEventListener("click", likeCard);
     deleteButton.addEventListener('click', deleteCard);
+    image.addEventListener("click", () => handleImageClick({ name, link }));
     return card;
 }
 
+// Функция лайка карточки
+export function likeCard(evt) {
+    evt.target.classList.toggle("card__like-button_is-active");
+}
+
 // @todo: Функция удаления карточки
-function deleteCard(event) {
+export function deleteCard(event) {
     const deleteItem = event.target.closest('.card');
     deleteItem.remove();
 }
-
-// @todo: Вывести карточки на страницу
-initialCards.forEach((item) => {
-    const card = createCard(item.name, item.link, deleteCard);
-    places.append(card);
-});
-
-    
--- webpack.config.js
+++ webpack.config.js
@@ -49,5 +49,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
 
-  ]
+  ],
+  devtool: 'source-map'
+
 } 
